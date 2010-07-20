443b4f022b67845c3130d02eb4bfb44ef3efb9f7
BenHall <ben@benhall.me.uk>
2010-07-20 23:53:18 +0100

    Error trying to fix

---
+++ SparkViewsInSubDirectories/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace n2CMS_Spark_Template.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
